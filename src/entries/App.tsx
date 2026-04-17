1dfca896d8b85f67f8ebf087a9c59a946e7a89c1
Martin Tiemens <martin@orange-juice.nl>
2026-04-17 12:53:04 +0200

    Remove temp code

--- src/entries/App.tsx
+++ src/entries/App.tsx
@@ -18,23 +18,23 @@ class App extends MithrilTsxComponent<{}> {
                             "Select a component to see it in action.",
                             "Happy testing!"]
 
-  private listingData: IListing = {
-      header: { title: "Listing title", heading: "h2" },
-      items: [
-        { title: "First Item",   content: "This is the first item content",   properties: {} },
-        { title: "Second Item",  content: "This is the second item content",  properties: { "Color": "Red" } },
-        { title: "Third Item",   content: "This is the third item content",   properties: { "Color": "Blue", "Size": "Medium" } },
-        { title: "Fourth Item",  content: "This is the fourth item content",  properties: { "Color": "Green", "Size": "Large", "Weight": 1 } },
-        { title: "Fifth Item",   content: "This is the fifth item content",   properties: {} },
-        { title: "Sixth Item",   content: "This is the sixth item content",   properties: { "Color": "Yellow" } },
-        { title: "Seventh Item", content: "This is the seventh item content", properties: { "Color": "Purple", "Size": "Small" } },
-        { title: "Eighth Item",  content: "This is the eighth item content",  properties: { "Color": "Orange", "Size": "XL", "Weight": 2 } },
-        { title: "Ninth Item",   content: "This is the ninth item content",   properties: {} },
-        { title: "Tenth Item",   content: "This is the tenth item content",   properties: { "Color": "Black" } },
-        { title: "Eleventh Item",content: "This is the eleventh item content",properties: { "Color": "White", "Size": "S" } },
-        { title: "Twelfth Item", content: "This is the twelfth item content", properties: { "Color": "Pink", "Size": "XXL", "Weight": 0.5 } },
-      ]
-    }
+  // private listingData: IListing = {
+  //     header: { title: "Listing title", heading: "h2" },
+  //     items: [
+  //       { title: "First Item",   content: "This is the first item content",   properties: {} },
+  //       { title: "Second Item",  content: "This is the second item content",  properties: { "Color": "Red" } },
+  //       { title: "Third Item",   content: "This is the third item content",   properties: { "Color": "Blue", "Size": "Medium" } },
+  //       { title: "Fourth Item",  content: "This is the fourth item content",  properties: { "Color": "Green", "Size": "Large", "Weight": 1 } },
+  //       { title: "Fifth Item",   content: "This is the fifth item content",   properties: {} },
+  //       { title: "Sixth Item",   content: "This is the sixth item content",   properties: { "Color": "Yellow" } },
+  //       { title: "Seventh Item", content: "This is the seventh item content", properties: { "Color": "Purple", "Size": "Small" } },
+  //       { title: "Eighth Item",  content: "This is the eighth item content",  properties: { "Color": "Orange", "Size": "XL", "Weight": 2 } },
+  //       { title: "Ninth Item",   content: "This is the ninth item content",   properties: {} },
+  //       { title: "Tenth Item",   content: "This is the tenth item content",   properties: { "Color": "Black" } },
+  //       { title: "Eleventh Item",content: "This is the eleventh item content",properties: { "Color": "White", "Size": "S" } },
+  //       { title: "Twelfth Item", content: "This is the twelfth item content", properties: { "Color": "Pink", "Size": "XXL", "Weight": 0.5 } },
+  //     ]
+  //   }
 
   view() {
     return (
@@ -58,7 +58,7 @@ class App extends MithrilTsxComponent<{}> {
             ))}
           </ul>
         </div>
-        <Listing {...this.listingData} />
+        {/* <Listing {...this.listingData} /> */}
       </div>
     )
   }
